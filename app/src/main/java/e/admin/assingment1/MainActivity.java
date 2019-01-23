bcc7073a86442b9326d0cc425446f15de49901a2
ShrutiAgrawal08 <shruti.agrawal_mca18@gla.ac.in>
2019-01-23 08:51:42 -0800

    add code

--- app/src/main/java/e/admin/assingment1/MainActivity.java
+++ app/src/main/java/e/admin/assingment1/MainActivity.java
@@ -1,41 +1,75 @@
 package e.admin.assingment1;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-     EditText editText1,editText2;
+
+
+    EditText editText1,editText2;
     Button button;
     int c=3;
     String s,s1;
 
-
-    int c=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-        editText1 = findViewById(R.id.edit);
-        editText2 = findViewById(R.id.edit2);
+
+        button=findViewById(R.id.button);
+        editText1=findViewById(R.id.edt);
+        editText2=findViewById(R.id.edt2);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 s=editText1.getText().toString();
+                s1=editText2.getText().toString();
+
+
+
+
+
+                if(s.equals(s1)){
+                    Toast.makeText(MainActivity.this,"password and username is correct",Toast.LENGTH_LONG).show();
+
+
+
+                }
+
+                else{
+
+                    if(c>0){
+
+                        Toast.makeText(MainActivity.this, "wrong password "+c+" attempt left", Toast.LENGTH_SHORT).show();
+                        c--;
+                    }
+                    else if(c==0){
+                        button.setEnabled(false);
+                    }
+
+                }
+
+
+
+
+
+
+
             }
         });
 
 
 
-         {
-            @Override
-            public void onClick(View v) {
 
 
 
@@ -43,10 +77,5 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
-            }
-        });
     }
 }
